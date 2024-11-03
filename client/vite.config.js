53a36e6e5d3613e55083a0d33bed64e19468eaad
Faruq <olowonlafaruq@gmail.com>
2024-11-03 09:50:59 -0800

    change

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',                   // Specifies the root directory where index.html is located
+  root: './cleint',                   // Specifies the root directory where index.html is located
   build: {
     outDir: 'dist',            // Output directory for the build (relative to the root)
     emptyOutDir: true,         // Clears the output directory before each build

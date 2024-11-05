0b7b29099be4e6cae891a73aacac00c2516ff89f
Faruq <olowonlafaruq@gmail.com>
2024-11-05 14:02:48 -0800

    changes

--- client/src/lib/axiosInstance.js
+++ client/src/lib/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // Determine the base URL based on the environment
-const baseUrl = import.meta.env.MODE === "development" ? 'http://localhost:5000/api' : '/api';
+const baseUrls = import.meta.env.MODE === "development" ? 'http://localhost:5000/api' : '/api';
 
 // Create an Axios instance with the base URL and credentials enabled
 export const axiosInstance = axios.create({
-    baseURL: baseUrl,
+    baseURL: baseUrls,
     withCredentials: true,
 });

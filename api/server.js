b646bb1a8980478cae878ff79692807dd7b0bad4
Faruq <olowonlafaruq@gmail.com>
2024-11-05 15:04:00 -0800

    change

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "https://job-finders.onrender.com",
     credentials: true
 }));
 app.use(express.json());
